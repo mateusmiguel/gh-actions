3a785466d99782eb1c558c7eb19989b7a0b58e5f
Mateus Miguel <webmateusmiguel@gmail.com>
2023-07-28 12:34:47 -0300

    testing dev

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>workflows separados</h1>
+      <h1>workflows separados - dev</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
