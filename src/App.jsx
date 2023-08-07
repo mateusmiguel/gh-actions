c809588365f19703a9f7e42f2554660c9784198b
Mateus Miguel <webmateusmiguel@gmail.com>
2023-08-07 19:58:15 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <button>
-          Button test!
+          Button test
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
